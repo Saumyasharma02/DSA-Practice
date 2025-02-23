6e34a111f0ca77294d8057681fd5f07675a77e6b
Saumya Sharma <149924114+Saumyasharma02@users.noreply.github.com>
2025-02-23 22:42:12 +0530

    Update que1.cpp

--- Heap/que1.cpp
+++ Heap/que1.cpp
@@ -1,1 +1,44 @@
+//Kth Largest Element in an Array
+
+//1.MAX HEAP
+#include <iostream>
+#include <vector>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+    int findKthLargest(vector<int>& nums, int k) {
+        priority_queue<int> pq;  // Max Heap by default
+
+        // Push all elements into the priority queue
+        for (int num : nums) {
+            pq.push(num);
+        }
+
+        // Pop elements until we reach the k-th largest
+        int num;
+        while (k > 0) {
+            num = pq.top();
+            pq.pop();
+            k--;
+        }
+        return num;
+    }
+};
+
+int main() {
+    Solution sol;
+
+    // Example input
+    vector<int> nums = {3, 2, 1, 5, 6, 4};
+    int k = 2;
+    int result = sol.findKthLargest(nums, k);
+    cout << "The " << k << "-th largest element is: " << result << endl;
+
+    return 0;
+}
+
+
 
