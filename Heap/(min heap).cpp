5fe752c08b4c2408a1c4ad2632aac28db5d20b57
Saumya Sharma <149924114+Saumyasharma02@users.noreply.github.com>
2025-02-28 22:37:11 +0530

    Update (min heap).cpp

--- Heap/(min heap).cpp
+++ Heap/(min heap).cpp
@@ -26,12 +26,4 @@ public:
     }
 };
 
-/*This C++ code finds the **k closest points to the origin (0,0) from a list of 2D points using a min-heap (priority queue). 
-The function `kClosest` takes a vector of points and an integer `k` as input and returns a vector of the `k` closest points based on their Euclidean distance 
-from the origin. 
-Instead of calculating the actual distance (which would require a square root operation), it uses the squared distance formula \(x^2 + y^2\) to avoid 
-unnecessary computationwhile maintaining accuracy for comparison. 
-Each point’s distance, along with its index, is stored as a pair in a min-heap (using a priority queue with `greater<>` comparator). 
-Since the heap stores elements in ascending order of distance, the smallest (closest) distance is always on top. 
-The algorithm then extracts the top `k` elements from the heap and adds the corresponding points to the result vector. 
-This approach ensures an efficient solution with a time complexity of **O(n log n)** due to heap insertions and extractions.*/
+
